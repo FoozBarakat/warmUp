9fb8831ccb16205eba1edc000aaefc6bbe44e25b
FoozBarakat <56113235+FoozBarakat@users.noreply.github.com>
2019-10-30 19:34:48 +0100

    warmup_11

--- warmUp11.js
+++ warmUp11.js
@@ -4,3 +4,21 @@
 // Create two unordered lists next to each other
 // Write a jQuery function that creates a new element in the left unordered list if the “left” button was pressed before the “create” button and to the right unordered list if the “right” button was pressed before the “create” button
 // Style the two unordered lists in CSS
+
+var x;
+
+$('#btnLeft').click( function() {
+	x = 'left';
+});
+
+$('#btnRight').click( function() {
+   x = 'right'	
+});
+
+$('#btnCreate').click( function() {
+	if (x==='left') {
+		$('#ulLeft').prepend('<li>' + 'left' + '</li>');
+	} else {
+		$('#ulRight').prepend('<li>' + 'right' + '</li>');
+	}	
+});

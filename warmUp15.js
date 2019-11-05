d5a7883390cb751660382d9b0161a0dff44e92d1
FoozBarakat <56113235+FoozBarakat@users.noreply.github.com>
2019-11-05 09:40:29 +0100

    warmUp15

--- warmUp15.js
+++ warmUp15.js
@@ -4,3 +4,21 @@
 //
 //
 // Note : you can`t use any built-in functions .
+
+function reverseStr(string) {
+	var hold = [];
+	var result = '';
+	for (var i = 0; i <= string.length; i++) {
+		if (string[i] === ' ') {
+			var word = string.slice(0, i+1);
+			hold.unshift(word);
+			string = string.replace(word, '');
+			i = 0;
+		}
+	}
+
+	result = string + ' ' + hold.join('');
+	return result;
+}
+
+console.log( reverseStr("hi there how is the weather today") );
